9b229096000868f9d5a59f57d10287853923e8d7
Lucas Almeida <lucas.almdsz@outlook.com>
2017-01-13 09:24:15 +0000

    Using list comprehensions

--- language_detector/main.py
+++ language_detector/main.py
@@ -4,18 +4,13 @@
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
 
-    # this variable represents how many words there are in the text for each language
-    word_counter = { lang['name'] : 0 for lang in languages }
-
-    # Remove special characters in text
-    character_list = [c for c in text if c.isdigit() or c.isalpha() or c == ' ']
+    character_list = [ c for c in text if c.isdigit() or c.isalpha() or c is ' ' ]
     word_list = "".join(character_list).split()
 
-    for lang in languages:
-        for word in word_list:
-            if word in lang['common_words']:
-                word_counter[lang['name']] += 1
+    results = { lang['name']:len([ word for word in word_list
+                                   if word in lang['common_words'] ])
+                for lang in languages }
 
-    return max(word_counter, key=word_counter.get)
+    return max(results, key=results.get)
+

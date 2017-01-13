bdd78f9b46f976a8db08d52c9232aebf02cbb637
Lucas Almeida <lucas.almdsz@outlook.com>
2017-01-13 05:21:57 +0000

    Initial commit

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,4 +5,17 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+
+    # this variable represents how many words there are in the text for each language
+    word_counter = { lang['name'] : 0 for lang in languages }
+
+    # Remove special characters in text
+    character_list = [c for c in text if c.isdigit() or c.isalpha() or c == ' ']
+    word_list = "".join(character_list).split()
+
+    for lang in languages:
+        for word in word_list:
+            if word in lang['common_words']:
+                word_counter[lang['name']] += 1
+
+    return max(word_counter, key=word_counter.get)

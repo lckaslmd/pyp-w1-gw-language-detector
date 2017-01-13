377ea76202e94011935a421d0e3ade993025efa6
Lucas Almeida <lucas.almdsz@outlook.com>
2017-01-13 10:01:42 +0000

    Transforming into lower the words of text

--- language_detector/main.py
+++ language_detector/main.py
@@ -6,7 +6,7 @@ def detect_language(text, languages):
     """Returns the detected language of given text."""
 
     character_list = [ c for c in text if c.isalpha() or c.isdigit() or c is ' ' ]
-    word_list = "".join(character_list).split()
+    word_list = "".join(character_list).lower().split()
 
     results = { lang['name']:len([ word for word in word_list
                                    if word in lang['common_words'] ])
